f2145755010c97e695fecba96e67cb250346e6eb
jzjian <86827451+jzjian@users.noreply.github.com>
2022-07-12 05:06:10 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, request, render_template
+import joblib
+
+
+# In[2]:
+
+
+app = Flask(__name__)
+
+
+# In[3]:
+
+
+@app.route("/", methods = ["GET","POST"])
+def index():
+    if request.method == "POST":
+        rates = float(request.form.get("rates"))
+        print(rates)
+        model1 = joblib.load("regression")
+        pred1 = model1.predict([[rates]])
+        model2 = joblib.load("decision_tree")
+        pred2 = model2.predict([[rates]])
+        return(render_template("index.html",result1=pred1, result2=pred2))
+    else:
+        return(render_template("index.html",result1="Waiting", result2="Waiting"))
+
+
+# In[ ]:
+
+
+if __name__=="__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
